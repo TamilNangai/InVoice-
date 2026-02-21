bdeb4206696a2e21bb6cf1a10dc7f7f8b4b26ad7
TamilNangaiK <k.tamilpirai@gmail.com>
2026-02-21 19:33:00 +0530

    complete: level bar and cards

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,10 +1,14 @@
+// import DashCards from "@/Components/Cards/DashCards"
 import Cards from "@/Components/Cards/Cards"
+
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Cards/>
+    <div className="w-full max-h-40 gap-[2%] flex justify-center items-center">
+      < Cards head="Total Revenue" symbol="$" amount={1000.00} cardhead="sanchez-regular text-[#000000]" cardamount="text-[32px]" />
+      < Cards head="Invoice Issued" amount={1200} cardhead="sanchez-regular text-[#000000]" cardamount="text-[32px]" />
+      < Cards head="Pending Payments" symbol="$" amount={1200.00} para="-- 25 Invoice Pending" cardhead="sanchez-regular text-[#000000]" cardamount="text-[32px]" cardpara="text-[16px] text-[#000000]  " />
+      < Cards head="Clients" amount={2400} cardhead="sanchez-regular text-[#000000]" cardamount="text-[32px]" />
     </div>
   )
 }
--- src/Pages/Reports.tsx
+++ src/Pages/Reports.tsx
@@ -1,10 +1,22 @@
+
 import Radiogroup from "@/Components/Nav/Radiogroup"
+import Cards from "@/Components/Cards/Cards"
+import Revenue from "@/Components/Cards/Revenue"
 
 
 const Reports = () => {
   return (
-    <div>
+    <div className="w-full">
         <Radiogroup />
+      <div className="w-full flex justify-center items-center ">
+        <div className="w-[50%] m-5  grid grid-cols-2 gap-[30px]  place-items-center">
+        < Cards head="Total Revenue" amount={50000.00} para="+15.% form last month" cardhead="iceberg-regular text-[#666666]" cardamount="text-[30px]" cardpara="text-[14px] text-[#34C759]" />
+        < Cards head="Paid Invoice" amount={145} para="90% Collection rate" cardhead="iceberg-regular text-[#666666]" cardamount="text-[30px]" cardpara="text-[14px] text-[#34C759]" />
+        < Cards head="Pending Amount" amount={50000.00} para="10 Invoice pending" cardhead="iceberg-regular text-[#666666]" cardamount="text-[30px]" cardpara="text-[14px]  text-[#FFCC00] " />
+        < Cards head="Overdue" amount={20000.00} para="3 invoice overdue" cardhead="iceberg-regular text-[#666666]" cardamount="text-[30px]" cardpara="text-[14px] text-[#FF383C]" />
+      </div>
+        <div className="w-[50%] mr-10"><Revenue/></div>
+      </div>
     </div>
   )
 }
