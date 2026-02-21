af070730eb2c48d666dc39b30dc8c1588a95951c
Atchayasri <atchayasribalasundaram@gmail.com>
2026-02-21 06:14:03 +0530

    link popup

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,33 @@ import Popup2 from "./assets/Popup2.png"
 import Popup3 from "./assets/Popup3.png"
 import Popup4 from "./assets/Popup4.png"
 import Create from "./Components/Form/Create"
+import Internship_invoice from "./Pages/Internship_invoice"
+import InvoicePage from "./Pages/InvoicePage"
+import Product_invoice from "./Pages/Product_invoice"
+import Products from "./Pages/Products"
+import Reports from "./Pages/Reports"
+import Service_invoice from "./Pages/Service_invoice"
+import Settings from "./Pages/Settings"
 function App() {
 
   return (
     <>
-      {/* <Buttons src={src} h1="Continue" />
-      <Std /> */}
-      {/* <Header
-        h1="Products & Inverntory"
+    <Internship_invoice/>
+    <InvoicePage/>
+    <Product_invoice/>
+    <Products/>
+    <Reports/>
+    <Settings/>
+      <Buttons src={src} h1="Continue" />
+      {/* <Std/> */}
+      <Header h1="Products & Inverntory"
         h2="Manage Your product catalog and service offerings."
-        h3="Add Product"
-        src={vectorw}
       />
-      <Create
+      <Popup
         popup1={Popup1}
         popup2={Popup2}
         popup3={Popup3}
         popup4={Popup4}
-        src1={vectorw}
-        src2={src}
 
         h1="Select Invoice Type"
         para1="Choose the type of invoice you want to create. "
@@ -43,14 +51,7 @@ billing and shipping"
 billing and shipping"
         h5="Other Invoice"
         para5="Customer or miscellaneous
-service billing"
-        h6="Add Product" /> */}
-      <Add h1="Add Products"
-        para1="Manage your product catalog and service offerings."
-        h2="Details"
-        h3="Product & Service Details"
-        h4="Product or Service Name"
-        h5="Product or Service ID" />
+service billing"/>
     </>
   )
 }
