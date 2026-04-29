e99fd08d4bf7f13dccf872c00e4f18da7f9bb9ca
swe1810 <you@example.com>
2026-04-29 19:20:12 +0530

    branches are combined

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,15 @@ import Reports from "./Pages/Reports"
 import Internship_invoice from "./Pages/Internship_invoice"
 import Service_invoice from "./Pages/Service_invoice"
 import Sidebar from "./Components/Nav/Sidebar"
-import { useEffect, useState } from "react";
+
 
 function App() {
 
-  useEffect(() => {
-    window.electron?.on("main-process-message", (data) => {
-      
-    })
-  }, [])
-
-  // const [mobileOpen, setMobileOpen] = useState(false);
 
   return (
     <>
       <HashRouter>
         <section className="w-screen min-h-screen max-h-screen flex">
-          {/* <div className="min-w-screen min-h-screen flex"> */}
-
-          
-        
-{/* 
-            <Sidebar
-              mobileOpen={mobileOpen}
-              onMobileClose={() => setMobileOpen(false)}
-            /> */}
           <Sidebar />
           <main className="flex-1 h-screen overflow-auto no-scrollbar">
             <Routes>
